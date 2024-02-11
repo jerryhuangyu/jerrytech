2b6c71a4eae03f9b73b4202708be94a6b6a584d0
jerryhuangyu <jerry0932619628@gmail.com>
2024-02-11 22:35:09 +0800

    feat: Create skillset section

--- components/sections/Skillset.tsx
+++ components/sections/Skillset.tsx
@@ -1,4 +1,19 @@
+import SectionTitle from "../SectionTitle";
+import SkillBadge from "../SkillBadge";
+import { skills } from "@/constants";
+
 const Skillset = () => {
-  return <div>Skillset</div>;
+  return (
+    <div className="section-wrap">
+      <SectionTitle title="Skillset" />
+      <div className="flex flex-wrap gap-2">
+        {skills.map((skill) => (
+          <div key={skill}>
+            <SkillBadge skill={skill} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default Skillset;
---
+++ constants/index.ts
@@ -0,0 +1,12 @@
+export const skills = [
+  "React",
+  "Next.js",
+  "JavaScript",
+  "TypeScript",
+  "HTML",
+  "CSS",
+  "Tailwindcss",
+  "Git",
+  "Framer-Motion",
+  "Supabase",
+];
