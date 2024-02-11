f8c3fa9b0ef402eb833f0d9b3423fee891610085
jerryhuangyu <jerry0932619628@gmail.com>
2024-02-11 22:34:07 +0800

    feat: Create about section

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -1,4 +1,23 @@
+import SectionTitle from "../SectionTitle";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="section-wrap">
+      <SectionTitle title="About" />
+      <p className="text-p">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+        libero officia eveniet id. Ab iusto dolor minus repudiandae laudantium
+        fuga sit odio vitae unde, deserunt perferendis culpa at quos, architecto
+        quaerat impedit laboriosam incidunt animi autem debitis. Quisquam
+        asperiores provident recusandae officia sapiente, at vel sunt velit
+        nulla inventore praesentium odio, dicta cumque saepe adipisci culpa id,
+        ipsa numquam non qui quidem doloremque aliquid delectus unde. Nobis sed
+        nemo quas autem, molestiae dolore, fuga debitis dolorem accusantium
+        aspernatur voluptates fugiat sequi suscipit. Modi ab nobis illum
+        corrupti ipsum officiis reiciendis quia iste nesciunt! Eligendi qui
+        porro a accusantium repudiandae quae.
+      </p>
+    </div>
+  );
 };
 export default About;
