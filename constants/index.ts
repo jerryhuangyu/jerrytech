1b9a2aa81e765f7f403b3929399c2536555783ff
jerryhuangyu <jerry0932619628@gmail.com>
2024-02-17 01:31:53 +0800

    feat: Create experience section

--- constants/index.ts
+++ constants/index.ts
@@ -10,3 +10,20 @@ export const skills = [
   "Framer-Motion",
   "Supabase",
 ];
+
+export const experiences = [
+  {
+    timestamp: "Sep 2023 - Present",
+    title: "Viind GmbH - Fullstack Developer",
+    detail:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, nameligendi minus quisquam inventore voluptatibus, accusantium eiuslaborum eos eum doloribus, itaque in architecto. Facilis suscipitearum ab, repellat culpa voluptate dicta omnis voluptatum est deserunttemporibus at dolorem vel?",
+    skills: ["Next.js", "React", "Redux"],
+  },
+  {
+    timestamp: "Nov 2022 - Aug 2023",
+    title: "Viind GmbH - Frontend Developer",
+    detail:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, nameligendi minus quisquam inventore voluptatibus, accusantium eiuslaborum eos eum doloribus, itaque in architecto. Facilis suscipitearum ab, repellat culpa voluptate dicta omnis voluptatum est deserunttemporibus at dolorem vel?",
+    skills: ["Nuxt.js", "Vue", "Redux"],
+  },
+];
--- sections/Experience.tsx
+++ sections/Experience.tsx
@@ -1,4 +1,23 @@
+import SectionTitle from "@/components/SectionTitle";
+import ExperienceDetail from "@/components/ExperienceDetail";
+import { experiences } from "@/constants";
+
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <div className="section-wrap">
+      <SectionTitle title="Experience" />
+      <div className="flex flex-col gap-8">
+        {experiences.map((ex) => (
+          <ExperienceDetail
+            timestamp={ex.timestamp}
+            title={ex.title}
+            detail={ex.detail}
+            skills={ex.skills}
+            key={ex.title}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 export default Experience;
