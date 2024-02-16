d1dd18fa8553f3cfdc465aa4b77046ab9e878f55
jerryhuangyu <jerry0932619628@gmail.com>
2024-02-17 01:31:25 +0800

    feat: Create experience detail

---
+++ components/ExperienceDetail.tsx
@@ -0,0 +1,32 @@
+import SkillBadge from "@/components/SkillBadge";
+
+type ExperienceDetailProps = {
+  timestamp: string;
+  title: string;
+  detail: string;
+  skills: string[];
+};
+
+const ExperienceDetail = ({
+  timestamp,
+  title,
+  detail,
+  skills,
+}: ExperienceDetailProps) => {
+  return (
+    <div className="flex">
+      <h3 className="text text-secondary mr-6 min-w-40 text-sm">{timestamp}</h3>
+      <div className="flex flex-col gap-2">
+        <h3 className="text text-primary text-base">{title}</h3>
+        <p className="text text-secondary text-sm">{detail}</p>
+        <div className="flex flex-wrap gap-2 pt-1">
+          {skills.map((skill) => (
+            <SkillBadge key={skill} skill={skill} highlight />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ExperienceDetail;
