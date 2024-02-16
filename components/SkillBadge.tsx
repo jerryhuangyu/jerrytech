2d7c417a3afff07bd522cb62fa1b0feed97cd18c
jerryhuangyu <jerry0932619628@gmail.com>
2024-02-17 01:30:53 +0800

    feat: Add highlight mode in skill badge

--- components/SkillBadge.tsx
+++ components/SkillBadge.tsx
@@ -1,10 +1,16 @@
 type SkillBadgeProps = {
   skill: string;
+  highlight?: boolean;
 };
 
-const SkillBadge = ({ skill }: SkillBadgeProps) => {
+const SkillBadge = ({ skill, highlight }: SkillBadgeProps) => {
+  const themeStyle = highlight
+    ? "border-[#C785EC] bg-[#3D2853] text-[#C785EC]"
+    : "border-[#333333] bg-[#232323] text-[#b9b9b9]";
   return (
-    <p className="text-nowrap rounded-sm border-[1px] border-[#333333] bg-[#232323] px-2 py-1 text-center text-sm font-light text-[#b9b9b9]">
+    <p
+      className={`text-nowrap rounded-sm border-[1px] px-2 py-1 text-center text-sm font-light ${themeStyle}`}
+    >
       {skill}
     </p>
   );
