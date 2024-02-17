38deedc207558d2fab7a34447e5e9eaf0854296f
jerryhuangyu <jerry0932619628@gmail.com>
2024-02-17 20:44:33 +0800

    feat: Add section title for project

--- sections/Projects.tsx
+++ sections/Projects.tsx
@@ -1,12 +1,16 @@
 import ProjectCard from "@/components/ProjectCard";
+import SectionTitle from "@/components/SectionTitle";
 import { projectCards } from "@/constants";
 
 const Projects = () => {
   return (
-    <div className="section-wrap flex flex-wrap justify-center gap-2">
-      {projectCards.map((card) => (
-        <ProjectCard card={card} />
-      ))}
+    <div className="section-wrap">
+      <SectionTitle title="Project" />
+      <div className="flex flex-wrap gap-2">
+        {projectCards.map((card) => (
+          <ProjectCard card={card} />
+        ))}
+      </div>
     </div>
   );
 };
