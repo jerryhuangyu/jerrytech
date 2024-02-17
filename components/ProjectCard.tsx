47ef29eaeebb0eace460d74fee5687c89c40dde1
jerryhuangyu <jerry0932619628@gmail.com>
2024-02-17 20:44:08 +0800

    style: Add hover effect to project card

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -9,7 +9,7 @@ type ProjectCardPorps = {
 
 const ProjectCard = ({ card }: ProjectCardPorps) => {
   return (
-    <div className="relative flex h-40 flex-1 flex-col items-center justify-around rounded border-[1px] border-[#333333] bg-gradient-to-br from-[#202020] to-[#151515] px-5 pb-5 pt-10">
+    <div className="relative flex h-40 flex-1 flex-col items-center justify-around rounded border-[1px] border-[#333333] bg-gradient-to-br from-[#202020] to-[#151515] px-5 pb-5 pt-10 duration-300 ease-in-out hover:border-purple-400 hover:border-opacity-60 hover:from-[#242424] hover:to-[#101010]">
       <div className="absolute right-0 top-0 flex gap-3 p-5">
         <IconLink icon={github} name="github" to={card.source} />
         <IconLink icon={link} name="github" to={card.demo} />
