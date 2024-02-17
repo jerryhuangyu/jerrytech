13506605b1e2fde35d884ddd6eff32001d3e9c01
jerryhuangyu <jerry0932619628@gmail.com>
2024-02-17 22:27:10 +0800

    feat: Add section title id for navigation usage

--- components/SectionTitle.tsx
+++ components/SectionTitle.tsx
@@ -3,6 +3,10 @@ type SectionTitleProps = {
 };
 
 const SectionTitle = ({ title }: SectionTitleProps) => {
-  return <h2 className="mb-5 text-2xl font-normal">{title}</h2>;
+  return (
+    <h2 className="mb-5 pt-28 text-2xl font-normal" id={title.toLowerCase()}>
+      {title}
+    </h2>
+  );
 };
 export default SectionTitle;
