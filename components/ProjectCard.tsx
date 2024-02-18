1e6ff65ea2fa080259ca41abe1363193c62c6977
jerryhuangyu <jerry0932619628@gmail.com>
2024-02-19 00:44:47 +0800

    style: Two columns above md

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -9,15 +9,17 @@ type ProjectCardPorps = {
 
 const ProjectCard = ({ card }: ProjectCardPorps) => {
   return (
-    <div className="relative flex h-40 flex-1 flex-col items-center justify-around rounded border-[1.5px] border-[#333333] bg-gradient-to-br from-[#202020] to-[#151515] px-5 pb-5 pt-10 duration-300 ease-in-out hover:border-purple-400 hover:border-opacity-60 hover:from-[#242424] hover:to-[#101010] sm:max-w-[49.6%]">
+    <div className="relative flex h-40 min-w-full flex-1 flex-col items-center justify-around rounded border-[1.5px] border-[#333333] bg-gradient-to-br from-[#202020] to-[#151515] px-5 pb-5 pt-10 duration-300 ease-in-out hover:border-purple-400 hover:border-opacity-60 hover:from-[#242424] hover:to-[#101010] md:min-w-[40%] md:max-w-[49.6%]">
       <div className="absolute right-0 top-0 flex gap-3 p-5">
         <IconLink icon={github} name="github" to={card.source} />
         <IconLink icon={link} name="github" to={card.demo} />
       </div>
       <h3 className="text-primary text-xl font-light">{card.name}</h3>
-      <div className="text-secondary flex gap-4 text-sm font-extralight">
+      <div className="text-secondary flex flex-wrap justify-center gap-3 tracking-tight ">
         {card.skills.map((s) => (
-          <p>{s}</p>
+          <p key={s} className="font-mono text-xs font-thin brightness-75">
+            {s}
+          </p>
         ))}
       </div>
     </div>
