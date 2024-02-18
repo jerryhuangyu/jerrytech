7effce092ec62ff8db66687f2cce370f5d19af89
jerryhuangyu <jerry0932619628@gmail.com>
2024-02-19 00:42:59 +0800

    feat: Add constants for about

--- sections/About.tsx
+++ sections/About.tsx
@@ -1,22 +1,11 @@
 import SectionTitle from "@/components/SectionTitle";
+import { about } from "@/constants";
 
 const About = () => {
   return (
     <div className="section-wrap">
       <SectionTitle title="About" />
-      <p className="text text-secondary text-sm">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-        libero officia eveniet id. Ab iusto dolor minus repudiandae laudantium
-        fuga sit odio vitae unde, deserunt perferendis culpa at quos, architecto
-        quaerat impedit laboriosam incidunt animi autem debitis. Quisquam
-        asperiores provident recusandae officia sapiente, at vel sunt velit
-        nulla inventore praesentium odio, dicta cumque saepe adipisci culpa id,
-        ipsa numquam non qui quidem doloremque aliquid delectus unde. Nobis sed
-        nemo quas autem, molestiae dolore, fuga debitis dolorem accusantium
-        aspernatur voluptates fugiat sequi suscipit. Modi ab nobis illum
-        corrupti ipsum officiis reiciendis quia iste nesciunt! Eligendi qui
-        porro a accusantium repudiandae quae.
-      </p>
+      <p className="text text-secondary text-sm">{about}</p>
     </div>
   );
 };
