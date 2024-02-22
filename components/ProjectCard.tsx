adcaaf662c948a742858dfb07382c862b5b916a7
jerryhuangyu <jerry0932619628@gmail.com>
2024-02-22 08:35:41 +0800

    fix: nested a elements are forbidden in HTML syntax

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -9,15 +9,10 @@ type ProjectCardPorps = {
 
 const ProjectCard = ({ card }: ProjectCardPorps) => {
   return (
-    <a
-      href={card.demo}
-      target="_blank"
-      className="relative flex h-40 min-w-full flex-1 flex-col items-center justify-around rounded border-[1.5px] border-[#333333] bg-gradient-to-br from-[#202020] to-[#151515] px-5 pb-5 pt-10 duration-300 ease-in-out hover:border-purple-400 hover:border-opacity-60 hover:from-[#242424] hover:to-[#101010] md:min-w-[40%] md:max-w-[49.6%]"
-      rel="noreferrer"
-    >
+    <div className="relative flex h-40 min-w-full flex-1 flex-col items-center justify-around rounded border-[1.5px] border-[#333333] bg-gradient-to-br from-[#202020] to-[#151515] px-5 pb-5 pt-10 duration-300 ease-in-out hover:border-purple-400 hover:border-opacity-60 hover:from-[#242424] hover:to-[#101010] md:min-w-[40%] md:max-w-[49.6%]">
       <div className="absolute right-0 top-0 flex gap-3 p-5">
         <IconLink icon={github} name="github" to={card.source} />
-        <IconLink icon={link} name="github" to={card.demo} />
+        <IconLink icon={link} name="demo" to={card.demo} />
       </div>
       <h3 className="text-xl font-light text-primary">{card.name}</h3>
       <div className="flex flex-wrap justify-center gap-3 tracking-tight text-secondary ">
@@ -27,7 +22,7 @@ const ProjectCard = ({ card }: ProjectCardPorps) => {
           </p>
         ))}
       </div>
-    </a>
+    </div>
   );
 };
 export default ProjectCard;
