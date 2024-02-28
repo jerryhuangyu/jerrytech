a7841118d18eacd6f64b75de5364c7a011fd41d4
jerryhuangyu <jerry0932619628@gmail.com>
2024-02-28 11:03:09 +0800

    feat: Add client-side icon link

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { Card } from '@/types';
-import IconLink from './IconLink';
+import ClientIconLink from '@/components/icons/ClientIconLink';
 import github from '@/images/github.svg';
 import link from '@/images/link.svg';
+import { Card } from '@/types';
 
 type ProjectCardPorps = {
   card: Card;
@@ -13,11 +13,11 @@ const ProjectCard = ({ card }: ProjectCardPorps) => {
   return (
     <div
       className="relative flex h-40 min-w-full flex-1 flex-col items-center justify-around rounded border-[1.5px] border-[#333333] bg-gradient-to-br from-[#202020] to-[#151515] px-5 pb-5 pt-10 duration-300 ease-in-out hover:border-purple-400 hover:border-opacity-60 hover:from-[#242424] hover:to-[#101010] md:min-w-[40%] md:max-w-[49.6%]"
-      onClick={() => window.open(card.demo, '_blank')}
+      onClick={() => {}}
     >
-      <div className="absolute right-0 top-0 flex gap-3 p-5">
-        <IconLink icon={github} name="github" to={card.source} />
-        <IconLink icon={link} name="demo" to={card.demo} />
+      <div className="absolute right-0 top-0 z-50 flex gap-3 p-5">
+        <ClientIconLink icon={github} name="github" to={card.source} />
+        <ClientIconLink icon={link} name="demo" to={card.demo} />
       </div>
       <h3 className="text-xl font-light text-primary">{card.name}</h3>
       <div className="flex flex-wrap justify-center gap-3 tracking-tight text-secondary ">
