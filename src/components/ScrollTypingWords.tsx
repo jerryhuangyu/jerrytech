fa0c8f8331ab56821350fb000b390bfe2c16abac
jerryhuangyu <jerry0932619628@gmail.com>
2024-03-12 13:01:40 +0800

    feat: Add words typing animation while scroll

---
+++ src/components/ScrollTypingWords.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { useRef } from 'react';
+import { gsap } from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import SplitType from 'split-type';
+gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(ScrollTrigger);
+
+type WordsAnimateProps = {
+  text: string;
+};
+
+const ScrollTypingWords = ({ text }: WordsAnimateProps) => {
+  const container = useRef(null);
+
+  useGSAP(
+    () => {
+      if (!container.current) return;
+      const splitTypes = new SplitType(container.current, {
+        types: ['chars'],
+        tagName: 'p'
+      });
+      gsap.from(splitTypes.chars, {
+        scrollTrigger: {
+          trigger: container.current,
+          start: 'top 85%',
+          end: 'top 30%',
+          scrub: true,
+          markers: false
+        },
+        opacity: 0.2,
+        stagger: 0.1
+      });
+    },
+    { scope: container, dependencies: [container] }
+  );
+
+  return <p ref={container}>{text}</p>;
+};
+export default ScrollTypingWords;
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,14 +1,15 @@
 import SectionTitle from '@/src/components/SectionTitle';
 import { useTranslations } from 'next-intl';
+import ScrollTypingWords from '../components/ScrollTypingWords';
 
 const About = () => {
   const t = useTranslations('Index');
   return (
     <div className="section-wrap">
       <SectionTitle title={t('sectionTitle.about')} />
-      <p className="text font-light text-secondary brightness-110 xl:text-[17px]">
-        {t('about')}
-      </p>
+      <div className="text font-light text-secondary brightness-110 xl:text-[17px]">
+        <ScrollTypingWords text={t('about')} />
+      </div>
     </div>
   );
 };
