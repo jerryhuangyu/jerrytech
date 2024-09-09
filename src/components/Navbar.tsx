cf427a87a157eaa5e0cf61faa3f6478edfb67b48
jerryhuangyu <jerry0932619628@gmail.com>
2024-09-10 01:44:46 +0800

    feat: navbar should get init state from url and popstate event

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,39 +1,39 @@
-import NavbarBtn from '@/src/components/NavbarBtn';
-import { RiHome5Line } from 'react-icons/ri';
-import { PiLightbulb } from 'react-icons/pi';
-import { BsSuitcase } from 'react-icons/bs';
-import { MdOutlineCases } from 'react-icons/md';
-import { useTranslations } from 'next-intl';
+'use client';
 
-const Navbar = () => {
-  const t = useTranslations();
-  const home = t('Index.sectionTitle.home');
-  const about = t('Index.sectionTitle.about');
-  const experience = t('Index.sectionTitle.experience');
-  const project = t('Index.sectionTitle.project');
+import NavbarBtn from '@/src/components/NavbarBtn';
+import { HOME_SECTIONS } from '@/types';
+import React, { useEffect } from 'react';
+import { updateStateFromURL } from '../store';
+
+type NavbarProps = {
+  buttons: {
+    title: string;
+    to: string;
+    sectionKey: HOME_SECTIONS;
+    icon: React.ReactNode;
+  }[];
+};
+
+const Navbar = ({ buttons }: NavbarProps) => {
+  useEffect(() => {
+    const handlePopState = () => {
+      updateStateFromURL();
+    };
+    window.addEventListener('popstate', handlePopState);
+    return () => window.removeEventListener('popstate', handlePopState);
+  }, [updateStateFromURL]);
 
   return (
     <div className="fixed top-8 z-50 grid grid-cols-2 gap-[0.5px] rounded-2xl bg-zinc-800/70 bg-clip-padding px-3 py-[6px] backdrop-blur sm:h-12 sm:grid-cols-4 sm:gap-1">
-      <NavbarBtn
-        to={`#${home.toLowerCase()}`}
-        name={home}
-        icon={<RiHome5Line />}
-      />
-      <NavbarBtn
-        to={`#${about.toLowerCase()}`}
-        name={about}
-        icon={<PiLightbulb />}
-      />
-      <NavbarBtn
-        to={`#${experience.toLowerCase()}`}
-        name={experience}
-        icon={<BsSuitcase />}
-      />
-      <NavbarBtn
-        to={`#${project.toLowerCase()}`}
-        name={project}
-        icon={<MdOutlineCases />}
-      />
+      {buttons.map((btn) => (
+        <NavbarBtn
+          key={btn.title}
+          to={btn.to}
+          name={btn.title}
+          sectionKey={btn.sectionKey}
+          icon={btn.icon}
+        />
+      ))}
     </div>
   );
 };
