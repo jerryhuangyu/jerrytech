906d9fb7b19b6cde1d91f16db65b65b4d27e1ec9
jerryhuangyu <jerry0932619628@gmail.com>
2024-11-02 21:41:11 +0800

    refactor: update animate timing

--- src/components/dnd/DndHint.tsx
+++ src/components/dnd/DndHint.tsx
@@ -12,6 +12,8 @@ import DndSkill from "./DndSkill"
 gsap.registerPlugin(useGSAP)
 gsap.registerPlugin(ScrollTrigger)
 
+const DND_HINT_DURATION_SEC = 2
+
 const _DndHint = () => {
 	const [dndHintComplete, setDndHintComplete] = useState(false)
 
@@ -20,21 +22,21 @@ const _DndHint = () => {
 			setDndHintComplete(true)
 			clearTimeout(job)
 			gsap.to(".dnd-hint-text", { opacity: 0, duration: 0.15 })
-		}, 1000)
+		}, 8000)
 	}
 
 	const showDndHintText = () => {
-		gsap.to(".dnd-hint-text", { opacity: 1, duration: 3, ease: "power3.out" })
+		gsap.to(".dnd-hint-text", { opacity: 1, duration: DND_HINT_DURATION_SEC + 2 })
 	}
 
 	useGSAP(() => {
 		gsap.to(".dnd-hint", {
 			scrollTrigger: { trigger: ".dnd-hint", start: "top 90%" },
-			x: 100,
+			x: 140,
 			y: -60,
 			rotate: 18,
-			duration: 3,
-			ease: "power3.out",
+			duration: DND_HINT_DURATION_SEC,
+			ease: "elastic.in(1,0.3)",
 			onStart: showDndHintText,
 			onComplete: hiddenDndHint,
 		})
@@ -44,8 +46,8 @@ const _DndHint = () => {
 		<>
 			<p
 				className={twMerge(
-					"dnd-hint-text [text-shadow:_0_4px_4px_rgb(216_180_254_/_0.8)]",
-					"absolute text-purple-300 font-bold text-md -top-24 left-[70px] rotate-[18deg] m-1 opacity-0",
+					"dnd-hint-text [text-shadow:_0_4px_4px_rgb(216_180_254_/_0.7)]",
+					"absolute text-purple-300 font-bold text-md -top-24 left-[110px] rotate-[18deg] m-1 opacity-0",
 					"before:absolute before:h-full before:w-full before:-z-10",
 					dndHintComplete && "opacity-0 duration-500 pointer-events-none",
 				)}
