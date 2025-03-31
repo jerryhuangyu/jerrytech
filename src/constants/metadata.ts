b901bc5c4ce78a7a723d8c59f4c07bcc1202a305
jerryhuangyu <jerry0932619628@gmail.com>
2025-03-31 21:45:26 +0800

    fix: update profile image URL in metadata

--- src/constants/metadata.ts
+++ src/constants/metadata.ts
@@ -2,5 +2,5 @@ export const webMeta = {
 	description:
 		"Hello. I'm Jerry Huang. A front-end developer. I'm passionate about creating experiences that are easy to use, accessible, and that meet the user's needs.",
 	title: "Jerry Huang | Profile",
-	image: "/og-preview.png",
+	image: "https://jerrytech.vercel.app/og-preview.png",
 }
