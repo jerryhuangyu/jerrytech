6c1532caed3d75775d5c614d5461ff016d4861fd
jerryhuangyu <jerry0932619628@gmail.com>
2025-03-31 21:39:31 +0800

    fix: update profile image path in metadata

--- src/constants/metadata.ts
+++ src/constants/metadata.ts
@@ -1,6 +1,6 @@
 export const webMeta = {
-  description:
-    "Hello. I'm Jerry Huang. A front-end developer. I'm passionate about creating experiences that are easy to use, accessible, and that meet the user's needs.",
-  title: 'Jerry Huang | Profile',
-  image: 'https://i.imgur.com/VRwVfVR.jpg'
-};
+	description:
+		"Hello. I'm Jerry Huang. A front-end developer. I'm passionate about creating experiences that are easy to use, accessible, and that meet the user's needs.",
+	title: "Jerry Huang | Profile",
+	image: "/og-preview.png",
+}
