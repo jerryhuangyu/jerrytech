be468dea3acf2d876a7d354308e546f05d0397a6
PoYu <68704871+jerryhuangyu@users.noreply.github.com>
2025-05-25 21:58:41 +0800

    [FEAT] add project: personal blog (#17)
    * chore: update indentation style from tab to space and adjust indent width
    
    * fix: update hero title and footer copyright year
    
    * feat: update tech blog link
    
    * feat: update logo
    
    * feat: auto show 3d model after finish loading
    
    * feat: add vercel configuration for caching headers
    
    * fix: remove preload to enable auto-rotate functionality
    
    * feat: add personal blog project details and image to portfolio
    
    * fix: remove source link from blog project details

--- src/components/HeroAsset.tsx
+++ src/components/HeroAsset.tsx
@@ -1,25 +1,15 @@
-'use client';
+"use client"
 
-import Image from 'next/image';
-import { lazy, useState, useTransition } from 'react';
-import { useIsMobile } from '../hooks/device';
+import Image from "next/image"
+import { Suspense, lazy } from "react"
+import { useIsMobile } from "../hooks/device"
 
-const HeroModel = lazy(() => import('@/src/components/canvas/Model'));
+const HeroModel = lazy(() => import("@/src/components/canvas/Model"))
 
-const HeroAsset = () => {
-  const [, startTransition] = useTransition();
-  const [load, setLoad] = useState(false);
-  const { isDesktop } = useIsMobile();
-
-  const handleImageClicked = () => {
-    startTransition(() => setLoad(true));
-  };
-
-  const HeroImageBtn = () => (
-    <div
-      className="relative flex h-full w-full justify-center"
-      onClick={handleImageClicked}
-    >
+const HeroImage = () => {
+  const { isDesktop } = useIsMobile()
+  return (
+    <div className="relative flex h-full w-full justify-center">
       <Image
         priority
         quality={isDesktop() ? 100 : 4}
@@ -27,10 +17,18 @@ const HeroAsset = () => {
         src="/author.png"
         width={3000}
         height={462}
-        style={{ objectFit: 'cover', width: 'auto', height: '100%' }}
+        style={{ objectFit: "cover", width: "auto", height: "100%" }}
       />
     </div>
-  );
-  return <>{load ? <HeroModel /> : <HeroImageBtn />}</>;
-};
-export default HeroAsset;
+  )
+}
+
+const HeroAsset = () => {
+  return (
+    <Suspense fallback={<HeroImage />}>
+      <HeroModel />
+    </Suspense>
+  )
+}
+
+export default HeroAsset
